8b8d229eb3562c439c13ac1bf03faf90cabb9f0c
Simon Renaud <srenaud@c2r8s5.42lausanne.ch>
2024-12-23 17:27:54 +0100

    pret pour l'eval

--- test/main_plus.c
+++ test/main_plus.c
@@ -6,7 +6,7 @@
 /*   By: srenaud <srenaud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 16:36:49 by srenaud           #+#    #+#             */
-/*   Updated: 2024/12/23 17:03:18 by srenaud          ###   ########.fr       */
+/*   Updated: 2024/12/23 17:25:20 by srenaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,25 @@ int	main(void)
 	write(1, "%%\n", 3);
 	write(1, "printf    = ", 12);
 	len = printf("%%");
-	printf("	len = %d\n", len);
+	printf("			len = %d\n", len);
 	write(1, "ft_printf = ", 12);
 	len = ft_printf("%%");
+	printf("			len = %d\n", len);
+	
+	write(1, "\n%%%%\n", 6);
+	write(1, "printf    = ", 12);
+	len = printf("%%%%");
+	printf("			len = %d\n", len);
+	write(1, "ft_printf = ", 12);
+	len = ft_printf("%%%%");
+	printf("			len = %d\n", len);
+	
+	write(1, "\n%s%d%c%X\n", 10);
+	write(1, "printf    = ", 12);
+	len = printf("%s %d '%c' %X","hello",42,'a',42);
+	printf("	len = %d\n", len);
+	write(1, "ft_printf = ", 12);
+	len = ft_printf("%s %d '%c' %X","hello",42,'a',42);
 	printf("	len = %d\n", len);
 
 	return (0);
